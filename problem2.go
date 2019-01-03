4ba7e32d9ed6746f3384ed57c5b4ca0e272132c0
Bobby Castleberry <bobby@castleberry-farms.com>
2019-01-02 20:34:50 -0800

    added problem 2 completed.

---
+++ problem2.go
@@ -0,0 +1,42 @@
+package projecteuler
+
+import "fmt"
+import "errors"
+
+func FibSums(limit int64, filter string) (res int64, err error) {
+	// Take a set of the Fibonacci up to the limit and sum the numbers together.
+	// You can use the filter arguement to filter what is summed by even numbers or odd numbers.
+	// If you pass nil to the filter it will not filter the results.
+	if filter != "even" && filter != "odd" && filter != "" {
+		return 0, errors.New("invalid filter arguement")
+	}
+	var lastfib int64
+	var curfib int64
+	var nextfib int64
+	var fibs []int64
+	curfib = 1
+	lastfib = 1
+	for curfib < limit {
+		// deal with the filter first
+		switch filter {
+		case "even":
+			if curfib%2 == 0 {
+				fibs = append(fibs, curfib)
+			}
+		case "odd":
+			if curfib%2 != 0 {
+				fibs = append(fibs, curfib)
+			}
+		default:
+			fibs = append(fibs, curfib)
+		}
+		// roll the numbers
+		nextfib = lastfib + curfib
+		lastfib = curfib
+		curfib = nextfib
+	}
+	for _, num := range fibs {
+		res = res + int64(num)
+	}
+	return res, nil
+}
---
+++ problem2_test.go
@@ -0,0 +1,31 @@
+package projecteuler
+
+import "testing"
+
+func TestFibSums(t *testing.T) {
+	tables := []struct {
+		filter string
+		limit  int64
+		result int64
+	}{
+		{"even", 100, 44},
+		{"odd", 100, 187},
+		{"", 100, 231},
+		{"even", 4000000, 16},
+	}
+	for pos, table := range tables {
+		res, err := FibSums(table.limit, table.filter)
+		if err != nil {
+			t.Errorf("error returned from function.  %v", err)
+		}
+		if res != table.result {
+			t.Errorf("Expected %d, got %d for test %d", table.result, res, pos+1)
+		}
+	}
+}
+
+func BenchmarkFibSums(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		FibSums(100000, "even")
+	}
+}
